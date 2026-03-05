f067cdb8dcd1c62b3ac3018c5b1499630ae824ee
Teodora916 <126460395+Teodora916@users.noreply.github.com>
2026-03-05 22:12:56 +0100

    AuthService

--- services/user-service/UserService.Application/DTOs/RefreshTokenResponse.cs
+++ services/user-service/UserService.Application/DTOs/RefreshTokenResponse.cs
@@ -3,5 +3,6 @@ namespace UserService.Application.DTOs;
 public record RefreshTokenResponse
 {
     public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public DateTime ExpiresAt { get; set; }
 }
