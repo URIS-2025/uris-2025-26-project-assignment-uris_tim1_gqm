107345ad808ac6e0148237040d210c74682fad91
andjelamrdja <mrdja.it7.2022@uns.ac.rs>
2026-03-08 21:23:12 +0100

    Added real data about authentificated user in sidebar-footer

--- client/src/app/layout/sidebar/sidebar.component.ts
+++ client/src/app/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
 import { HasPermissionDirective } from '../../core/permissions/has-permission.directive';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/auth/auth.service';
 
 interface NavItem {
     label: string;
@@ -32,4 +33,18 @@ const NAV_ITEMS: NavItem[] = [
 })
 export class SidebarComponent {
     navItems = NAV_ITEMS;
+    public auth = inject(AuthService);
+
+    get initials(): string {
+        const user = this.auth.currentUser;
+        if (!user) return '?';
+        return `${user.firstName[0]}${user.lastName[0]}`.toUpperCase();
+    }
+
+    get fullName(): string {
+        const user = this.auth.currentUser;
+        if (!user) return '';
+        return `${user.firstName} ${user.lastName}`;
+    }
 }
+
