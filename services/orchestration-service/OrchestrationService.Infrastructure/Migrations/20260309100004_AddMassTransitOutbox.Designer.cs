946a53f9f1f46dcff2a28f38ac7898750bab90fd
anicmarko <anic.it36.2022@gmail.com>
2026-03-09 11:51:33 +0100

    chore: add EF migrations for MassTransit outbox and configure Docker infrastructure

--- services/orchestration-service/OrchestrationService.Infrastructure/Migrations/20260309100004_AddMassTransitOutbox.Designer.cs
+++ services/orchestration-service/OrchestrationService.Infrastructure/Migrations/20260309100004_AddMassTransitOutbox.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrchestrationService.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using OrchestrationService.Infrastructure.Persistence;
 namespace OrchestrationService.Infrastructure.Migrations
 {
     [DbContext(typeof(OrchestrationDbContext))]
-    partial class OrchestrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260309100004_AddMassTransitOutbox")]
+    partial class AddMassTransitOutbox
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
